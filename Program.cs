fa9a60ac1a27258883350362d4af133ba983573e
unknown <animesh.sakorikar@gmaiil.com>
2023-11-29 15:00:25 -0800

    initial commit, executable to run nvim

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Running unity nvim loader");
+
+// ProcessStartInfo psi = new ProcessStartInfo{
+// 	FileName = "cmd.exe", 
+// 	RedirectStandardInput = true, 
+// 	RedirectStandardOutput = true,
+// 	RedirectStandardError = true,
+// 	UseShellExecute = false,
+// 	CreateNoWindow = false,
+// 	WindowStyle = ProcessWindowStyle.Normal
+// };
+
+ProcessStartInfo psi = new ProcessStartInfo{
+	FileName = "nvim.exe", 
+	// RedirectStandardInput = true, 
+	// RedirectStandardOutput = true,
+	// RedirectStandardError = true,
+	// UseShellExecute = false,
+	// CreateNoWindow = false,
+	// WindowStyle = ProcessWindowStyle.Normal
+};
+
+Process process = new Process {StartInfo = psi};
+process.Start();
+process.WaitForExit();
+process.Close();
