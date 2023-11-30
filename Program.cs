fee345ae74869ccaf7614d81a833150d513de439
unknown <animesh.sakorikar@gmail.com>
2023-11-29 16:35:09 -0800

    feat: accept command line arguments from unity editor

--- Program.cs
+++ Program.cs
@@ -1,26 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
-Console.WriteLine("Running unity nvim loader");
-
-// ProcessStartInfo psi = new ProcessStartInfo{
-// 	FileName = "cmd.exe", 
-// 	RedirectStandardInput = true, 
-// 	RedirectStandardOutput = true,
-// 	RedirectStandardError = true,
-// 	UseShellExecute = false,
-// 	CreateNoWindow = false,
-// 	WindowStyle = ProcessWindowStyle.Normal
-// };
+string[] nvimCommandLineArgs = Environment.GetCommandLineArgs();
+string nvimCommandLineArgsConcat = string.Join(" ", nvimCommandLineArgs, 1, nvimCommandLineArgs.Length-1);
 
 ProcessStartInfo psi = new ProcessStartInfo{
 	FileName = "nvim.exe", 
-	// RedirectStandardInput = true, 
-	// RedirectStandardOutput = true,
-	// RedirectStandardError = true,
-	// UseShellExecute = false,
-	// CreateNoWindow = false,
-	// WindowStyle = ProcessWindowStyle.Normal
+	Arguments = nvimCommandLineArgsConcat
 };
 
 Process process = new Process {StartInfo = psi};
