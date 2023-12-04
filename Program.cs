a90de495e60bc082c615aa9ee670ce7d15af3bfe
unknown <animesh.sakorikar@gmail.com>
2023-12-04 14:59:06 -0800

    fix(nvim-remote): fix bug where attempting to access same nvim remote more than once gave fatal error

--- Program.cs
+++ Program.cs
@@ -3,11 +3,12 @@ using System.IO.Pipes;
 
 class ConsoleApp
 {
+	private const string PipeRegistryFileName = "NamedPipeRegistry.txt";
+
 	static void Main(string[] args) {
 		string pipeName = @"\\.\pipe\NvimPipe.1111.0";
 
-		if (IsPipeOpen(pipeName)) {
-			Console.WriteLine($"Pipe {pipeName} already exists");
+		if (IsPipeRegistered(pipeName)) {
 			OpenFileNvimPipe(pipeName, args);
 		}
 		else {
@@ -16,28 +17,73 @@ class ConsoleApp
 		}
 	}
 
-	static bool IsPipeOpen(string pipeName) {
-		try {
-			using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut))
-			{
-				pipeClient.Connect(1000);
-				return pipeClient.IsConnected;
+	static string GetPipeRegistryFilePath() {
+		return Path.Combine(Directory.GetCurrentDirectory(), PipeRegistryFileName);
+	}
+
+	static void CreatePipeRegistryFileIfNotExists (string registryFilePath) {
+		if (!File.Exists(registryFilePath)) {
+			try {
+				File.WriteAllText(registryFilePath, string.Empty);
+			}
+			catch (Exception ex) {
+				Console.WriteLine($"Error creating registry file: {ex.Message}");
 			}
 		}
-		catch (TimeoutException) {
-			return false;
+	}
+
+	static bool IsPipeRegistered(string pipeName) {
+		string registryFilePath = GetPipeRegistryFilePath();
+
+		CreatePipeRegistryFileIfNotExists(registryFilePath);
+
+		string[] lines = File.ReadAllLines(registryFilePath);
+		foreach (string line in lines) {
+			if (line.Equals(pipeName, StringComparison.OrdinalIgnoreCase)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	static void RegisterPipe(string pipeName) {
+		string registryFilePath = GetPipeRegistryFilePath();
+
+		CreatePipeRegistryFileIfNotExists(registryFilePath);
+
+		try {
+			File.AppendAllText(registryFilePath, $"{pipeName}{Environment.NewLine}");
 		}
 		catch (Exception ex) {
-			Console.WriteLine($"Error checking pipe status: {ex.Message}");
-			return false;
+			Console.WriteLine($"Error creating registry file: {ex.Message}");
+		}
+	}
+
+	static void DeregisterPipe(string pipeName) {
+		string registryFilePath = GetPipeRegistryFilePath();
+		CreatePipeRegistryFileIfNotExists(registryFilePath);
+
+		try {
+			string[] lines = File.ReadAllLines(registryFilePath);
+			lines = lines.Where(line => !line.Equals(pipeName, StringComparison.OrdinalIgnoreCase)).ToArray();
+			File.WriteAllLines(registryFilePath, lines);
+		}
+		catch (Exception ex) {
+			Console.WriteLine($"Error creating registry file: {ex.Message}");
 		}
 	}
 
 	static void CreateNamedPipe(string pipeName) {
 		using (NamedPipeServerStream pipeServer = new NamedPipeServerStream(pipeName, PipeDirection.InOut)) {
 			Console.WriteLine($"Named pipe '{pipeName}' created.");
+			RegisterPipe(pipeName);
+			Console.WriteLine($"Named pipe '{pipeName}' registered.");
 			LaunchNvim(pipeName);
+			Console.WriteLine($"Launched nvim");
 		}
+		DeregisterPipe(pipeName);
+		Console.WriteLine($"Neovim Instance closed, pipe closed and deregistered");
 	}
 
 	static void LaunchNvim (string pipeName) {
@@ -71,29 +117,13 @@ class ConsoleApp
 		ProcessStartInfo psi = new ProcessStartInfo{
 			FileName = "cmd.exe",
 			Arguments = nvimCommand,
-			// RedirectStandardInput = true,
-			// RedirectStandardOutput = true,
-			// RedirectStandardError = true,
 			UseShellExecute = true,
 			CreateNoWindow = false
 		};
 
 		using (Process process = new Process {StartInfo = psi}) {
 			process.Start();
+			process.Close();
 		}
 	}
 }
-
-// string[] nvimCommandLineArgs = Environment.GetCommandLineArgs();
-// string nvimCommandLineArgsConcat = string.Join(" ", nvimCommandLineArgs, 1, nvimCommandLineArgs.Length-1);
-// nvimCommandLineArgsConcat = "--remote" + " "  + nvimCommandLineArgsConcat;
-//
-// ProcessStartInfo psi = new ProcessStartInfo{
-// 	FileName = "nvim.exe", 
-// 	Arguments = nvimCommandLineArgsConcat
-// };
-//
-// Process process = new Process {StartInfo = psi};
-// process.Start();
-// process.WaitForExit();
-// process.Close();
